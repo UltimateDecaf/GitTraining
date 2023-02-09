359390031b9032d3dff9d27ec664e75935c3aa6d
UltimateDecaf <ultimatedecaf@gmail.com>
2023-02-09 16:16:40 +0400

    - Added sayHello() function; - Scanner class imported to receive input from user.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("I'm using Git!");
+        sayHello();
+    }
+
+    public static void sayHello(){
+        System.out.print("What is your name? >> ");
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Hello, " + keyboard.nextLine());
     }
 }
